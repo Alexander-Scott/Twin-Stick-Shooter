dc3dbb35274273b2b5776842c975f6b027c06f9f
Alex Scott <alexowns2009@Hotmail.co.uk>
2016-06-22 22:16:23 +0100

    Project rename
    Project rename

--- Twin_Stick_Shooter/Intermediate/Build/Win64/UE4Editor/Development/Twin_Stick_Shooter/PCH.Twin_Stick_Shooter.h.cpp
+++
@@ -1,1 +0,0 @@
-#include "E:\Unreal Engine Projects\Twin-Stick-Shooter\Twin_Stick_Shooter\Source\Twin_Stick_Shooter\Twin_Stick_Shooter.h"
--- Twin_Stick_Shooter/Source/Twin_Stick_Shooter/Twin_Stick_Shooter.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Twin_Stick_Shooter.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Twin_Stick_Shooter, "Twin_Stick_Shooter" );
